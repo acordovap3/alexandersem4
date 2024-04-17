de7054b15647259c84b0b0d93999239ff25bcc0f
acordovap3 <166524315+acordovap3@users.noreply.github.com>
2024-04-17 00:21:47 +0000

    commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 const Main = () => {
     return <main>
-        <p>main</p>
+        <form>
+              <div class="form-group">
+                <label for="inputEmail">Correo</label>
+                <input type="email" class="form-control" id="inputEmail" placeholder="Correo"></input>
+              </div>
+              <div class="form-group">
+                <label for="inputPassword">Contrasena</label>
+                <input type="password" class="form-control" id="inputPassword" placeholder="Contrasena"></input>
+              </div>
+              <div class="form-group">
+                <label class="form-check-label"><input type="checkbox"> Recuerdamelo</label>
+              </div>
+              <button type="submit" class="btn btn-primary">Iniciar Sesion</button>
+            </form>
     </main>;
 }
 
