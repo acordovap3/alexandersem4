cb47df9490c0eda49c142808e897f0c1c671e414
acordovap3 <166524315+acordovap3@users.noreply.github.com>
2024-04-23 16:18:05 +0000

    commit

--- src/footer.js
+++ src/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <p><a class="link-opacity-100" href="https://www.facebook.com/UpaoPeru/?locale=es_LA">Facebook</a></p>
     </div>
     <div class="col">
-            Simulacion hecha por Alexander Cordova
+        <div class="p-3 mb-2 bg-primary-subtle text-primary-emphasis">Simulacion hecha por Alexander Cordova</div>
     </div>
     <div class="col">
         <p><a class="link-opacity-100" href="https://www.instagram.com/upao_peru/?hl=es">Instagram</a></p>
