8131db4ea808b6f76258f2f7d716bb58800f4529
acordovap3 <166524315+acordovap3@users.noreply.github.com>
2024-04-23 15:25:03 +0000

    commit

--- src/aside.js
+++ src/aside.js
@@ -3,7 +3,7 @@ const Aside = () => {
         <main class="container">
           <div class="row">
             <h1>Revisa nuestros servicios!</h1>
-            <img src="https://static2.upao.edu.pe/5/1690415310PRINCIPAL%20(13).jpg" width="300" height="450"></img>
+            <img src="https://static2.upao.edu.pe/5/1690415310PRINCIPAL%20(13).jpg" width="500" height="450"></img>
           </div>
         </main>
     </aside>;
