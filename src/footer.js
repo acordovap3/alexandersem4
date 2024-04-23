9d731ead244eb6d3161b8320448b602512059271
acordovap3 <166524315+acordovap3@users.noreply.github.com>
2024-04-23 15:14:51 +0000

    commit

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return <footer>
         <p></p>
-        <p>Simulación hecha por Alexander Eyner Cordova Plasencia</p>
+        <p>                        Simulación hecha por Alexander Eyner Cordova Plasencia</p>
     </footer>;
 }
 
